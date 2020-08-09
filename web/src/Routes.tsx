d021489a4f2564f8edc78239a166f706b7a83801
BenNeighbour <benneighbour007@gmail.com>
2020-08-09 14:22:37 +0100

    Cleanup file structure(s)

---
+++ web/src/Routes.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './App.css';
+
+interface Props { }
+
+const Routes: React.FC<Props> = (props) => {
+  return (
+    <div className="container">
+    </div>
+  );
+}
+
+export default Routes;
