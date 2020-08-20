ec862cb6f1025695bc6e651d3467437d94be0646
BenNeighbour <benneighbour007@gmail.com>
2020-08-20 19:16:38 +0100

    Making the search bar component have a Formik Form

--- web/src/components/Common/SearchBar/SearchBar.tsx
+++ web/src/components/Common/SearchBar/SearchBar.tsx
@@ -1,18 +1,16 @@
 import * as React from "react";
 import { RouteComponentProps } from "react-router-dom";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import {
-  KeyboardDatePicker,
-  MuiPickersUtilsProvider,
-} from "@material-ui/pickers";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
+import SearchBarForm from "./SearchBarForm";
 
 interface Props extends RouteComponentProps<any> {}
 
 const SearchBar: React.FC<Props> = (props) => {
+  const [errors, setErrors] = React.useState("");
+  
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <div
@@ -40,91 +38,15 @@ const SearchBar: React.FC<Props> = (props) => {
               padding: 0,
             }}
           >
-            <div
-              style={{
-                paddingTop: "0.5vh",
-                paddingBottom: "0.5vh",
-                flex: "1 0 0",
-                paddingRight: "1vw",
-                paddingLeft: "2vw",
-              }}
-            >
-              {/* Material Standard Inuput field */}
-              <TextField fullWidth focused label="Location" />
-            </div>
+            <SearchBarForm
+              errors=""
+              location={props.location}
+              history={props.history}
+              match={props.match}
+              onSubmit={() => {
 
-            <div
-              style={{
-                paddingTop: "0.5vh",
-                paddingBottom: "0.5vh",
-                paddingRight: "1vw",
               }}
-            >
-              <KeyboardDatePicker
-                style={{
-                  overflowY: "hidden"
-                }}
-                label="Check in"
-                focused
-                fullWidth
-                format="dd/MM/yy"
-                margin="none"
-                // onBlur={() => onBlur(value ? new Date(value).toISOString() : null)}
-                onChange={() => {
-                  console.log("£h");
-                }}
-                value={new Date()}
-              />
-            </div>
-
-            <div
-              style={{
-                paddingTop: "0.5vh",
-                paddingBottom: "0.5vh",
-                paddingRight: "1vw",
-              }}
-            >
-              <KeyboardDatePicker
-                style={{
-                  overflowY: "hidden"
-                }}
-                label="Check out"
-                focused
-                fullWidth
-                format="dd/MM/yy"
-                margin="none"
-                // onBlur={() => onBlur(value ? new Date(value).toISOString() : null)}
-                onChange={() => {
-                  console.log("£h");
-                }}
-                value={new Date()}
-              />
-            </div>
-
-            <div
-              style={{
-                paddingTop: "0.5vh",
-                paddingBottom: "0.5vh",
-                paddingRight: "1vw",
-              }}
-            >
-              <Button
-                style={{
-                  borderRadius: "35px",
-                  width: "min-content",
-                  padding: "6px",
-                  marginLeft: "auto",
-                  boxShadow: "none",
-                  textTransform: "none",
-                }}
-                color="primary"
-                variant="contained"
-                className="profile-button"
-                fullWidth
-              >
-                Search
-              </Button>
-            </div>
+            />
           </CardContent>
         </Card>
       </div>
